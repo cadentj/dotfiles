44e0778c4357b1c562ca71a6613f0c4711595492
cadentj <caden.juang@gmail.com>
2025-02-15 21:41:02 +0000

    Merge aliases to install

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 uv pip install -U "huggingface_hub[cli]"
-huggingface-cli login --token $HF_TOKEN
+source /root/.venv/bin/activate
+huggingface-cli login # This will prompt for token
+
+apt-get update
+apt-get install -y tmux
+
+# Add aliases to bashrc
+echo "alias gc='git commit -m'" >> /root/.bashrc
+echo "alias tma='tmux attach -t'" >> /root/.bashrc
+echo "export HF_HOME=/workspace/hf" >> /root/.bashrc
+source /root/.bashrc
--- setup_aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Add aliases to bashrc
-echo "alias gc='git commit -m'" >> ~/.bashrc
-echo "alias tma='tmux attach -t'" >> ~/.bashrc
-source ~/.bashrc
