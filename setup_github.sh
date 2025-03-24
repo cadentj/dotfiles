92c9e7b4a170cbb6d2622e43aa376dd36263d884
cadentj <caden.juang@gmail.com>
2025-03-24 17:20:29 +0000

    fixes

--- setup_github.sh
+++ setup_github.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install -y gh
+
 # Input arguments
 email=${1:-"caden.juang@gmail.com"}
 name=${2:-"cadentj"}
@@ -9,15 +11,9 @@ github_url=${3:-""}
 git config --global user.email "$email"
 git config --global user.name "$name"
 
-# Set GitHub token from environment variable
-if [ -n "$RUNPOD_GH_TOKEN" ]; then
-    git config --global credential.helper '!f() { echo "username=${name}"; echo "password=${RUNPOD_GH_TOKEN}"; }; f'
-else
-    echo "Warning: RUNPOD_GH_TOKEN environment variable not set"
-fi
-
 # Optional: Store credentials
 git config --global credential.helper store
 
-# Include the rest of the script someday...
-# https://github.com/jplhughes/dotfiles/blob/master/runpod/setup_github.sh
+# Set GitHub token for GitHub CLI
+gh auth login --with-token < $RUNPOD_GH_TOKEN
+
