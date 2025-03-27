51b4122f026e855e39f881a2e574a15b0bcdb6fe
cadentj <caden.juang@gmail.com>
2025-03-27 19:29:56 +0000

    use my sparsify fork

--- current.sh
+++ current.sh
@@ -5,7 +5,7 @@ git clone https://github.com/ndif-team/nnsight.git
 git clone https://github.com/apple/ml-cross-entropy.git
 git clone https://github.com/cadentj/autointerp.git
 git clone https://github.com/cadentj/OpenRLHF.git
-git clone https://github.com/EleutherAI/sparsify.git
+git clone https://github.com/cadentj/sparsify.git
 git clone https://cadentj:$RUNPOD_GH_TOKEN@github.com/HelenaCasademunt/steering-finetuning.git
 
 cd steering-finetuning && git checkout caden && bash setup.sh
