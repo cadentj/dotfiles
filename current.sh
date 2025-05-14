e41519c265fcee856b569fd6eef03e3ca21aa7bd
cadentj <caden.juang@gmail.com>
2025-05-14 03:37:47 +0000

    not using everything atm

--- current.sh
+++ current.sh
@@ -1,16 +1,18 @@
 source dotfiles/runpod.sh
 
 # Repos I'm using at the moment
-git clone https://github.com/ndif-team/nnsight.git
-git clone https://github.com/apple/ml-cross-entropy.git
-git clone https://github.com/cadentj/autointerp.git
-git clone https://github.com/cadentj/OpenRLHF.git
-git clone https://github.com/cadentj/sparsify.git
-git clone https://github.com/cadentj/emergent-misalignment.git
-git clone https://cadentj:$RUNPOD_GH_TOKEN@github.com/HelenaCasademunt/steering-finetuning.git
+# git clone https://github.com/ndif-team/nnsight.git
+# git clone https://github.com/apple/ml-cross-entropy.git
+# git clone https://github.com/cadentj/autointerp.git
+# git clone https://github.com/cadentj/OpenRLHF.git
+# git clone https://github.com/cadentj/sparsify.git
+# git clone https://github.com/cadentj/emergent-misalignment.git
+# git clone https://cadentj:$RUNPOD_GH_TOKEN@github.com/HelenaCasademunt/steering-finetuning.git
 
-cd /root/steering-finetuning && git checkout caden && bash setup.sh
-cd /root/sparsify && git checkout ssae && uv pip install -e .
-cd /root/autointerp && git checkout dash && uv pip install -e .
+# cd /root/steering-finetuning && git checkout caden && bash setup.sh
+# cd /root/sparsify && git checkout ssae && uv pip install -e .
+# cd /root/autointerp && git checkout dash && uv pip install -e .
+
+uv pip install wandb
 
 wandb login $RUNPOD_WB_TOKEN
