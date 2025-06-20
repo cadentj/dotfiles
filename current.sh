f0bc720b3d9b12b2e2786f13ef5822253d65881a
cadentj <caden.juang@gmail.com>
2025-06-20 18:41:39 +0000

    current workspace

--- current.sh
+++ current.sh
@@ -1,10 +1,11 @@
 source dotfiles/runpod.sh
 
 # Repos I'm using at the moment
-git clone https://github.com/ndif-team/nnsight.git
 git clone https://github.com/cadentj/autointerp.git
+git clone https://github.com/cadentj/steering-finetuning.git
 
 cd /root/autointerp && git checkout dev && uv pip install -e .
+cd /root/steering-finetuning && git checkout cleanup && uv pip install -r requirements.txt
 
 uv pip install wandb
 
