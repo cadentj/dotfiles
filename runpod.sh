233740f3a00813977840e7080b298b84ae4fb0b4
cadentj <caden.juang@gmail.com>
2026-01-28 21:20:40 +0000

    simplify

--- runpod.sh
+++ runpod.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 1) Install uv and create virtual environment
+# 1) Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv python install 3.11
-uv venv
 
 # 2) Setup github
 bash dotfiles/setup_github.sh
@@ -15,21 +12,3 @@ apt-get install -y tmux
 
 # 4) Add aliases
 echo "source dotfiles/aliases.sh" >> /root/.bashrc
-
-# 5) Set up environment variables in workspace
-echo "export HF_HOME=/workspace/hf" >> /root/.bashrc
-echo "export HF_HUB_ENABLE_HF_TRANSFER=1" >> /root/.bashrc
-
-# Reload .bashrc
-source /root/.bashrc
-
-# 6) Set up huggingface cache at /workspace/hf
-uv pip install -U "huggingface_hub[cli]"
-uv pip install -U "huggingface-hub[hf-transfer]"
-source /root/.venv/bin/activate
-
-if [ -z "$RUNPOD_HF_TOKEN" ]; then
-    huggingface-cli login --add-to-git-credential
-else
-    huggingface-cli login --token $RUNPOD_HF_TOKEN --add-to-git-credential
-fi
